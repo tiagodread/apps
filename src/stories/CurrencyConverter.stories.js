50e6f354c59f70c65e5afaf7ac414761493d0ea9
Tiago Góes <tiago.goes2009@gmail.com>
2022-06-03 19:33:06 -0300

    Storybook

--- src/stories/CurrencyConverter.stories.js
+++ src/stories/CurrencyConverter.stories.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
-import FromCurrency from '../components/Exchange/FromCurrency/FromCurrency';
+import Exchange from '../components/Exchange/Exchange.js';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Exchange/FromCurrency',
-  component: FromCurrency,
+  title: 'Exchange/Exchange',
+  component: Exchange,
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template = (args) => <FromCurrency {...args} />;
+const Template = (args) => <Exchange {...args} />;
 
-export const USD = Template.bind({});
+export const CurrencyConverter = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-USD.args = {
+CurrencyConverter.args = {
   fromCurrency: 'USD',
+  toCurrency: 'BRL',
 };
