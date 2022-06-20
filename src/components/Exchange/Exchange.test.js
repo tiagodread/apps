f261aeff74750362c0615b8800da318f6d2f0efe
Tiago Góes <tiago.goes2009@gmail.com>
2022-06-20 20:23:29 -0300

    Unskip tests

--- src/components/Exchange/Exchange.test.js
+++ src/components/Exchange/Exchange.test.js
@@ -1,77 +1,58 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import Exchange from './Exchange';
+import { render, screen, fireEvent } from "@testing-library/react";
+import Exchange from "./Exchange";
 
-describe('Exchange', () => {
-    it('renders without crashing', () => {
-        render(<Exchange />);
-    });
+describe("Exchange", () => {
+  it("renders without crashing", () => {
+    render(<Exchange />);
+  });
 
-    it('Should match the snapshot', () => {
-        const { container } = render(<Exchange />);
-        expect(container).toMatchSnapshot();
-    });
+  it("Should match the snapshot", () => {
+    const { container } = render(<Exchange />);
+    expect(container).toMatchSnapshot();
+  });
 
-    it('renders the correct initial currency state', () => {
-        render(<Exchange fromCurrency="CAD" toCurrency='EUR' />);
-        const fromCurrency = screen.getByTestId('from-currency');
-        expect(fromCurrency.value).toBe('CAD');
-        const toCurrency = screen.getByTestId('to-currency');
-        expect(toCurrency.value).toBe('EUR');
-    });
+  it("renders the correct initial currency state", () => {
+    render(<Exchange fromCurrency="CAD" toCurrency="EUR" />);
+    const fromCurrency = screen.getByTestId("from-currency");
+    expect(fromCurrency.value).toBe("CAD");
+    const toCurrency = screen.getByTestId("to-currency");
+    expect(toCurrency.value).toBe("EUR");
+  });
 
-    it('should change currency state when the user changes the from currency', () => {
-        render(<Exchange fromCurrency="CAD" toCurrency='EUR' />);
-        const fromCurrency = screen.getByTestId('from-currency');
-        expect(fromCurrency.value).toBe('CAD');
-        fromCurrency.value = 'USD';
-        expect(fromCurrency.value).toBe('USD');
-    });
+  it("should change currency state when the user changes the from currency", () => {
+    render(<Exchange fromCurrency="CAD" toCurrency="EUR" />);
+    const fromCurrency = screen.getByTestId("from-currency");
+    expect(fromCurrency.value).toBe("CAD");
+    fromCurrency.value = "USD";
+    expect(fromCurrency.value).toBe("USD");
+  });
 
-    it('should change currency state when the user changes the to currency', () => {
-        render(<Exchange fromCurrency="CAD" toCurrency='EUR' />);
-        const toCurrency = screen.getByTestId('to-currency');
-        expect(toCurrency.value).toBe('EUR');
-        toCurrency.value = 'USD';
-        expect(toCurrency.value).toBe('USD');
-    });
+  it("should change currency state when the user changes the to currency", () => {
+    render(<Exchange fromCurrency="CAD" toCurrency="EUR" />);
+    const toCurrency = screen.getByTestId("to-currency");
+    expect(toCurrency.value).toBe("EUR");
+    toCurrency.value = "USD";
+    expect(toCurrency.value).toBe("USD");
+  });
 
-    it('should show an window alert if the value is undefined or 0', () => {
-        jest.spyOn(window, 'alert').mockImplementation(() => { });
-        render(<Exchange fromCurrency="CAD" toCurrency='EUR' />);
-        const convertButton = screen.getByTestId('convert');
-        fireEvent.click(convertButton);
-        expect(window.alert).toHaveBeenCalled();
-        const enteredValue = screen.getByTestId('enteredValue');
-        enteredValue.value = '0';
-        fireEvent.click(convertButton);
-        expect(window.alert).toHaveBeenCalled();
-    })
+  it("should show an window alert if the value is undefined or 0", () => {
+    jest.spyOn(window, "alert").mockImplementation(() => {});
+    render(<Exchange fromCurrency="CAD" toCurrency="EUR" />);
+    const convertButton = screen.getByTestId("convert");
+    fireEvent.click(convertButton);
+    expect(window.alert).toHaveBeenCalled();
+    const enteredValue = screen.getByTestId("enteredValue");
+    enteredValue.value = "0";
+    fireEvent.click(convertButton);
+    expect(window.alert).toHaveBeenCalled();
+  });
 
-    it.skip('Should fetch the exchange rate when the user clicks the convert button', async () => {
-        jest.spyOn(window, 'fetch').mockImplementation(() => {
-            return Promise.resolve({
-                json: () => Promise.resolve({
-                    rates: {
-                        CAD: 1.5,
-                        EUR: 1.5
-                    }
-                })
-            });
-        }
-        );
-        const handleConvertedValue = jest.fn();
-        render(<Exchange fromCurrency="CAD" toCurrency='EUR' />);
-        const enteredValue = screen.getByTestId('enteredValue');
-        fireEvent.change(enteredValue, { target: { value: '5' } })
-        waitFor(() => {
-
-            const convertButton = screen.getByTestId('convert');
-            fireEvent.click(convertButton);
-            expect(window.fetch).toHaveBeenCalled();
-        })
-
-
-    })
-
+  it("Should fetch the exchange rate when the user clicks the convert button", async () => {
+    render(<Exchange fromCurrency="CAD" toCurrency="EUR" />);
+    const enteredValue = screen.getByTestId("enteredValue");
+    fireEvent.change(enteredValue, { target: { value: "5" } });
+    const convertButton = screen.getByTestId("convert");
+    fireEvent.click(convertButton);
+    await screen.findByTestId("exchange-result");
+  });
 });
-
