662e2e2978b2cce29293e803052e3c899727cbbf
Tiago Góes <tiago.goes2009@gmail.com>
2022-07-26 23:54:13 -0300

    Fix broken test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-it('renders learn react link', () => {
+it("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/💱 Currency Converter/i);
+  const linkElement = screen.getByText(/Currency Converter/i);
   expect(linkElement).toBeInTheDocument();
 });
